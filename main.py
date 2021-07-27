08c207033204f2f924be9de96274b7caa5c3d848
GNovaisL <84047213+GNovaisL@users.noreply.github.com>
2021-07-27 08:49:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import pygame
 from pygame.locals import *
 from sys import exit
 from random import randint
-#from pygame.image import load
 
 pygame.init()
-
+class PacMan(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.sprites = []
+        self.sprites.append()
 class Freeze(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -23,19 +26,21 @@ class Freeze(pygame.sprite.Sprite):
         self.atual = 0
         self.image = self.sprites[self.atual]
         self.image = pygame.transform.scale(self.image, (32 * 2, 32 * 2))
+
         self.rect = self.image.get_rect()
         self.rect.topleft = 100, 100
+
+
 
 todas_as_sprites = pygame.sprite.Group()
 freeze = Freeze()
 todas_as_sprites.add(freeze)
 
-
 altura = 650
 largura = 1300
 
-x_pac = 0
-y_pac = 0
+posx_pac = 20
+posy_pac = 20
 
 pac_movex = 0
 pac_movey = 0
@@ -46,16 +51,16 @@ y_coin = randint(50, 450)
 
 fonte = pygame.font.SysFont("PixelMix", 40, True, True)
 
+
 points = 0
 #CRIANDO A JANELA
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Rota de Fuga")
 relogio = pygame.time.Clock()
-#background = load('Imagem_a_ser_colocada.png')
 
 while True:
     relogio.tick(400)
-    tela.fill((0, 0, 0)) #substituir tla.fill por tela.blit(background, (0, 0))
+    tela.fill((0, 0, 0))
     mensagem = f"Pontos: {points}"
     texto = fonte.render(mensagem, True, (255, 255, 255))
 
@@ -68,29 +73,25 @@ while True:
             if event.key == K_a:
                 pac_movex = -speed_pac
                 pac_movey = 0
-                x_pac = x_pac - pac_movex
+                posx_pac = posx_pac - pac_movex
             if event.key == K_d:
                 pac_movex = speed_pac
                 pac_movey = 0
-                x_pac = x_pac + pac_movex
+                posx_pac = posx_pac + pac_movex
             if event.key == K_w:
                 pac_movey = -speed_pac
                 pac_movex = 0
-                y_pac = y_pac - pac_movey
+                posy_pac = posy_pac - pac_movey
             if event.key == K_s:
                 pac_movey = speed_pac
                 pac_movex = 0
-                y_pac = y_pac + pac_movey
-    x_pac = x_pac + pac_movex
-    y_pac = y_pac + pac_movey
+                posy_pac = posy_pac + pac_movey
+    posx_pac = posx_pac + pac_movex
+    posy_pac = posy_pac + pac_movey
 
 #CRIANDO OBJETOS
+    pacman = pygame.draw.rect(tela, (255, 255, 0), (posx_pac, posy_pac, 30, 30))
     coin = pygame.draw.rect(tela, (255, 255, 255), (x_coin, y_coin, 10, 10))
-
-    pacman = pygame.draw.rect(tela, (255, 255, 0), (x_pac, y_pac, 30, 30))
-
-    todas_as_sprites.draw(tela)
-    todas_as_sprites.update()
 
     linha_esq = pygame.draw.line(tela, (0, 0, 255), (0, 0), (0, 650), 20)
     linha_sup = pygame.draw.line(tela, (0, 0, 255), (0, 0), (1300, 0), 20)
@@ -104,12 +105,12 @@ while True:
     tela.blit(texto, (450, 30))
 
     if pacman.colliderect(linha_esq):
-        x_pac = x_pac + 10
+        posx_pac = posx_pac + 10
     if pacman.colliderect(linha_dir):
-        x_pac = x_pac - 10
+        posx_pac = posx_pac - 10
     if pacman.colliderect(linha_sup):
-        y_pac = y_pac + 10
+        posy_pac = posy_pac + 10
     if pacman.colliderect(linha_inf):
-        y_pac = y_pac - 10
+        posy_pac = posy_pac - 10
 
     pygame.display.flip()
