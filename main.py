4cc45bf48a211bfa08d672edd15c5a40350bcec1
kauansous <83986645+kauansous@users.noreply.github.com>
2021-08-02 15:14:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,116 +1,60 @@
 import pygame
 from pygame.locals import *
 from sys import exit
-from random import randint
 
-pygame.init()
-class PacMan(pygame.sprite.Sprite):
+largura= 1000
+altura= 400
+Speed= 10
+
+class Nave(pygame.sprite.Sprite):
+
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.sprites = []
-        self.sprites.append()
-class Freeze(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.sprites = []
-        self.sprites.append(pygame.image.load("data/sprite_00.png"))
-        self.sprites.append(pygame.image.load("data/sprite_02.png"))
-        self.sprites.append(pygame.image.load("data/sprite_03.png"))
-        self.sprites.append(pygame.image.load("data/sprite_05.png"))
-        self.sprites.append(pygame.image.load("data/sprite_06.png"))
-        self.sprites.append(pygame.image.load("data/sprite_07.png"))
-        self.sprites.append(pygame.image.load("data/sprite_09.png"))
-        self.sprites.append(pygame.image.load("data/sprite_10.png"))
-        self.sprites.append(pygame.image.load("data/sprite_13.png"))
-        self.sprites.append(pygame.image.load("data/sprite_14.png"))
+        self.image_run=[pygame.image.load('Imagens/Nave.png')]
         self.atual = 0
-        self.image = self.sprites[self.atual]
-        self.image = pygame.transform.scale(self.image, (32 * 2, 32 * 2))
+        self.image= self.image_run[self.atual]
+        self.image=pygame.transform.scale(self.image,(309//4,163//4))
 
         self.rect = self.image.get_rect()
-        self.rect.topleft = 100, 100
+        self.rect.topleft = 50, 160
 
+    def update(self,*args):
+        def move_player(self):
+            key=pygame.key.get_pressed()
+            if key[pygame.K_w]:
+                self.rect[1] -= Speed
+            if key[pygame.K_s]:
+                self.rect[1] += Speed
+            if key[pygame.K_d]:
+                self.rect[0] += Speed
+            if key[pygame.K_a]:
+                self.rect[0] -= Speed
+            self.image = pygame.transform.scale(self.image, [309//4,163//4])
+        move_player(self)
 
+todas_as_sprites= pygame.sprite.Group()
+nave=Nave()
+todas_as_sprites.add(nave)
 
-todas_as_sprites = pygame.sprite.Group()
-freeze = Freeze()
-todas_as_sprites.add(freeze)
+pygame.init()
 
-altura = 650
-largura = 1300
+pygame.mixer.music.set_volume(0.12)
+musica=pygame.mixer.music.load('Músicas/Snes.mp3')
+pygame.mixer.music.play(-1)
 
-posx_pac = 20
-posy_pac = 20
+tela=pygame.display.set_mode((largura,altura))
+pygame.display.set_caption('Rota de Fuga')
+background=pygame.image.load('Imagens/Universo.jpg')
+background=pygame.transform.scale(background,(largura,altura))
 
-pac_movex = 0
-pac_movey = 0
-speed_pac = 0.7
-
-x_coin = randint(50, 600)
-y_coin = randint(50, 450)
-
-fonte = pygame.font.SysFont("PixelMix", 40, True, True)
-
-
-points = 0
-#CRIANDO A JANELA
-tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption("Rota de Fuga")
-relogio = pygame.time.Clock()
-
+clock=pygame.time.Clock()
 while True:
-    relogio.tick(400)
-    tela.fill((0, 0, 0))
-    mensagem = f"Pontos: {points}"
-    texto = fonte.render(mensagem, True, (255, 255, 255))
-
+    clock.tick(25)
+    tela.blit(background, (0, 0))
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type==QUIT:
             pygame.quit()
             exit()
-
-        if event.type == KEYDOWN:
-            if event.key == K_a:
-                pac_movex = -speed_pac
-                pac_movey = 0
-                posx_pac = posx_pac - pac_movex
-            if event.key == K_d:
-                pac_movex = speed_pac
-                pac_movey = 0
-                posx_pac = posx_pac + pac_movex
-            if event.key == K_w:
-                pac_movey = -speed_pac
-                pac_movex = 0
-                posy_pac = posy_pac - pac_movey
-            if event.key == K_s:
-                pac_movey = speed_pac
-                pac_movex = 0
-                posy_pac = posy_pac + pac_movey
-    posx_pac = posx_pac + pac_movex
-    posy_pac = posy_pac + pac_movey
-
-#CRIANDO OBJETOS
-    pacman = pygame.draw.rect(tela, (255, 255, 0), (posx_pac, posy_pac, 30, 30))
-    coin = pygame.draw.rect(tela, (255, 255, 255), (x_coin, y_coin, 10, 10))
-
-    linha_esq = pygame.draw.line(tela, (0, 0, 255), (0, 0), (0, 650), 20)
-    linha_sup = pygame.draw.line(tela, (0, 0, 255), (0, 0), (1300, 0), 20)
-    linha_inf = pygame.draw.line(tela, (0, 0, 255), (0, 650), (1300, 650), 20)
-    linha_dir = pygame.draw.line(tela, (0, 0, 255), (1300, 0), (1300, 650), 20)
-
-    if pacman.colliderect(coin):
-        points = points + 1
-        x_coin = randint(50, 600)
-        y_coin = randint(50, 450)
-    tela.blit(texto, (450, 30))
-
-    if pacman.colliderect(linha_esq):
-        posx_pac = posx_pac + 10
-    if pacman.colliderect(linha_dir):
-        posx_pac = posx_pac - 10
-    if pacman.colliderect(linha_sup):
-        posy_pac = posy_pac + 10
-    if pacman.colliderect(linha_inf):
-        posy_pac = posy_pac - 10
-
-    pygame.display.flip()
+    todas_as_sprites.draw(tela)
+    todas_as_sprites.update()
+    pygame.display.update()
